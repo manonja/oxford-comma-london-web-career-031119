4127c778de051ed5f3d94492e2e6143ae493fce7
manon jacquin <manon.jacquin@gmail.com>
2019-02-21 11:21:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
 
+  if array.length >= 3
+    # creates the last connector with the last element of the array
+    newConnector = "and #{array[-1]}"
+    # remove the last element of the array so we can replace it with the newConnector created
+    array.pop()
+    # push the newConnector to our array and convert it to a string separated by commas
+    return array.push(newConnector).join(", ")
+  else
+    # convert the array to a string with and for any array < 3
+  return array.join(" and ")
+  end
+
 end
