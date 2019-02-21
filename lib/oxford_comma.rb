946a40ecfaa16f444d6a3ca4025bc71068b15f6a
manon jacquin <manon.jacquin@gmail.com>
2019-02-21 10:59:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
-  if array.length > 2
-  return array.join("and")
+  if array.length >= 3
+  puts [array[0...array.length - 2], array[array.length- 2..array.length - 1]].
+  map { |e| e.join ' and ' }.
+  join(', ')
+  else
+  puts array.join(" and ")
+  end
+
 end
