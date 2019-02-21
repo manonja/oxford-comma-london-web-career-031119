f6ba19f1103cdb1649a0716b9ad5ded8623ceacc
manon jacquin <manon.jacquin@gmail.com>
2019-02-21 10:59:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.length >= 3
-  puts [array[0...array.length - 2], array[array.length- 2..array.length - 1]].
+  [array[0...array.length - 2], array[array.length- 2..array.length - 1]].
   map { |e| e.join ' and ' }.
   join(', ')
   else
-  puts array.join(" and ")
+  array.join(" and ")
   end
 
 end
